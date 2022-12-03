7b1b27f2822b1d945b1ead014177122c96a3afc2
atanda0x <atandanafiu21@gmail.com>
2022-12-03 09:02:29 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@ethersproject/abi");
+require("@ethersproject/providers");
+require("@typechain/hardhat");
+require("@typechain/ethers-v5");
+require("@typechain/hardhat");
+require("@nomiclabs/hardhat-ethers")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
